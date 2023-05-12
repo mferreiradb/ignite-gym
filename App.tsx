b73c923a9bf98806d750e74fbb261b62db93e305
mferreiradb <mauhmg@gmail.com>
2023-05-12 20:39:08 -0300

    Implemented SignUp screen

--- App.tsx
+++ App.tsx
@@ -10,7 +10,7 @@ import {
 import { THEME } from './src/theme';
 import { NativeBaseProvider } from 'native-base';
 import { Loading } from '@components/Loading';
-import { SignIn } from '@screens/SignIn';
+import { SignUp } from '@screens/SignUp';
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
@@ -23,7 +23,7 @@ export default function App() {
         translucent
       />
 
-      {fontsLoaded ? <SignIn /> : <Loading />}
+      {fontsLoaded ? <SignUp /> : <Loading />}
     </NativeBaseProvider>
   );
 }
---
+++ src/screens/SignUp.tsx
@@ -0,0 +1,53 @@
+import { VStack, Image, Text, Center, Heading, ScrollView } from 'native-base';
+// Heading pode ser utilizado para textos em destaque, como titulos
+
+import ImgBg from '@assets/background.png';
+import Logo from '@assets/logo.svg';
+import { Input } from '@components/Input';
+import { Button } from '@components/Button';
+
+// VStack é uma view que organiza os componentes em coluna
+
+// Estamos utilizando o componentes Image para definir nossa imagem no bg, pois o componente nos fornece maior flexibilidade do que o BackgroundImage
+
+export function SignUp() {
+  return (
+    <ScrollView
+      contentContainerStyle={{ flexGrow: 1 }}
+      showsVerticalScrollIndicator={false}
+    >
+      <VStack px={10} flex={1} bg="gray.700">
+        <Image
+          source={ImgBg}
+          alt="Pessoas treinando"
+          resizeMode="contain"
+          position="absolute"
+        />
+        <Center my={24}>
+          <Logo />
+          <Text color="gray.100">Treine sua menste e seu corpo</Text>
+        </Center>
+        <Center>
+          <Heading fontFamily="heading" color="gray.100" mb={6} fontSize="xl">
+            Crie sua conta
+          </Heading>
+          <Input placeholder="Nome" placeholderTextColor="gray.300" />
+          <Input
+            autoCapitalize="none"
+            keyboardType="email-address"
+            placeholder="E-mail"
+            placeholderTextColor="gray.300"
+          />
+          <Input
+            secureTextEntry
+            placeholder="Senha"
+            placeholderTextColor="gray.300"
+          />
+          <Button title="Criar e acessar" />
+        </Center>
+
+        <Button mt={24} title="Voltar para o login" variant="outline" />
+      </VStack>
+    </ScrollView>
+  );
+}
